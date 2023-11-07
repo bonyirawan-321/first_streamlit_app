65c96fdca05be0ab840638273f3825eaa3f15c69
bonyirawan-321 <150125738+bonyirawan-321@users.noreply.github.com>
2023-11-07 16:25:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text('🥣 Omega 3 Blueberry Otameal')
 streamlit.text('🥗 Kale Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled free Range-Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
